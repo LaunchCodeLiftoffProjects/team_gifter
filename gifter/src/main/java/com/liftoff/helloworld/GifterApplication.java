830c9c9fb8476a44893c0ebe35043114b61c9bf0
Jordyn Crawford <jordynacrawford@gmail.com>
2020-12-04 16:51:19 -0600

    initial commit

---
+++ gifter/src/main/java/com/liftoff/helloworld/GifterApplication.java
@@ -0,0 +1,13 @@
+package com.liftoff.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifterApplication.class, args);
+	}
+
+}
