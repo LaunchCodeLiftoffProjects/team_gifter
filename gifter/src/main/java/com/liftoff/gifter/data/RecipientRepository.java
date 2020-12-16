7d9fc933fc8b39f9edd50dc4311280be395fb97f
davidschmutz <74635294+davidschmutz@users.noreply.github.com>
2020-12-16 07:01:12 -0600

    Initial commit of persistent recipients

--- gifter/src/main/java/com/liftoff/gifter/data/RecipientRepository.java
+++ gifter/src/main/java/com/liftoff/gifter/data/RecipientRepository.java
@@ -1,13 +1,11 @@
 package com.liftoff.gifter.data;
 
 import com.liftoff.gifter.models.Recipient;
-import com.liftoff.gifter.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface RecipientRepository extends CrudRepository<Recipient, Integer> {
-
-    Recipient findByFirstName(String firstName);
-
-    Recipient findByLastName(String lastName);
-
 }
