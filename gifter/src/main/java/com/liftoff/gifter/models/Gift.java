1e60c14538844aeede70311ec6dbcb740999cc4e
cathy <cathy.pattie@gmail.com>
2021-01-20 17:55:07 -0600

    No walk out of it

--- gifter/src/main/java/com/liftoff/gifter/models/Gift.java
+++ gifter/src/main/java/com/liftoff/gifter/models/Gift.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Entity
 public class Gift extends AbstractEntity {
 
+    private String title;
     private String description;
     private String price;
     private String link;
@@ -18,22 +19,28 @@ public class Gift extends AbstractEntity {
 
     }
 
-    public Gift(String description, String price, String link, List<Recipient> recipients) {
+    public Gift(String title,String description, String price, String link, List<Recipient> recipients) {
+        this.title = title;
         this.description = description;
         this.price = price;
         this.link = link;
         this.recipients = recipients;
     }
 
+    public String getTitle() {return title; }
+
+    public void setTitle(String title) {this.title = title; }
 
     public String getDescription() {return description; }
 
     public void setDescription(String description) {this.description = description; }
+
     public String getPrice() { return price; }
 
     public void setPrice(String price) {
         this.price = price;
     }
+
     public String getLink() { return link; }
 
     public void setLink(String link) {
